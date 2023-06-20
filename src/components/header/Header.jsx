0103b6c2a958a161e76c46edaac44f66295f25f9
Ullasachar16 <ullasachar16@gmail.com>
2023-06-20 15:10:54 +0530

    added transition for close and open navbar bottom

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <i className="uil uil-times nav_close"></i>
+                    <i className="uil uil-times nav_close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
                 <div className="nav_toggle" onClick={() => showMenu(!Toggle)}>
                     <i className="uil uil-apps"></i>
