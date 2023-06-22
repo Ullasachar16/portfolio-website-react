3e2094c8dc3bc7e850a0358ce214b0c26614195d
Ullasachar16 <ullasachar16@gmail.com>
2023-06-22 12:52:01 +0530

    changes in skills content

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -2,7 +2,69 @@ import React from 'react'
 
 const Frontend = () => {
     return (  
-        <div className="skills_content"></div>
+        <div className="skills_content">
+            <h3 className="skills_title"></h3>
+
+            <div className="skills_box">
+                <div className="skills_group">
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+                </div>
+
+                <div className="skills_group">
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+
+                    <div className="skills_data">
+                       <i className="bx bx-badge-check"></i> 
+
+                       <div>
+                        <h3 className="skills_name"></h3>
+                        <span className="skills_level"></span>
+                       </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
     )
 }
 
