a698311dba388e638658ba2b80913488c794dd44
Ullasachar16 <ullasachar16@gmail.com>
2023-06-22 13:00:40 +0530

    formatted content

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,71 +1,62 @@
 import React from 'react'
 
 const Backend = () => {
-  return (
-    <div className="skills_content">
-            <h3 className="skills_title"></h3>
+    return (
+        <div className="skills_content">
+            <h3 className="skills_title">Backend Developer</h3>
 
             <div className="skills_box">
                 <div className="skills_group">
                     <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
+                        <i className="bx bx-badge-check"></i>
 
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
+                        <div>
+                            <h3 className="skills_name">Java</h3>
+                            <span className="skills_level">Advanced</span>
+                        </div>
                     </div>
 
                     <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
+                        <i className="bx bx-badge-check"></i>
 
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
+                        <div>
+                            <h3 className="skills_name">Node JS</h3>
+                            <span className="skills_level">Basic</span>
+                        </div>
                     </div>
 
                     <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
+                        <i className="bx bx-badge-check"></i>
 
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
+                        <div>
+                            <h3 className="skills_name">Python</h3>
+                            <span className="skills_level">Intermediate</span>
+                        </div>
                     </div>
                 </div>
 
                 <div className="skills_group">
                     <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
+                        <i className="bx bx-badge-check"></i>
 
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
+                        <div>
+                            <h3 className="skills_name">My SQL</h3>
+                            <span className="skills_level">Intermediate</span>
+                        </div>
                     </div>
 
                     <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
+                        <i className="bx bx-badge-check"></i>
 
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
+                        <div>
+                            <h3 className="skills_name">Firebase</h3>
+                            <span className="skills_level">Intermediate</span>
+                        </div>
                     </div>
-
-                    <div className="skills_data">
-                       <i className="bx bx-badge-check"></i> 
-
-                       <div>
-                        <h3 className="skills_name"></h3>
-                        <span className="skills_level"></span>
-                       </div>
-                    </div>
-                </div>    
+                </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default Backend;
