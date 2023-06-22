43a69b66cff22353afa688e39e84547912e6ac42
Ullasachar16 <ullasachar16@gmail.com>
2023-06-22 12:06:16 +0530

    added box icons for about page

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const Info = () => {
-  return (
-    <div>Info</div>
-  )
+    return (
+        <div className="about_info grid">
+            <div className="about_box">
+                <i className="bx bx-award about_icon"></i>
+                <h3 className="about_title">Experience</h3>
+                <span className="about_subtitle">8 Years Working</span>
+            </div>
+
+            <div className="about_box">
+                <i className="bx bx-briefcase-alt about_icon"></i>
+                <h3 className="about_title">Completed</h3>
+                <span className="about_subtitle">48 + Projects</span>
+            </div>
+
+            <div className="about_box">
+                <i className="bx bx-support about_icon"></i>
+                <h3 className="about_title">Support</h3>
+                <span className="about_subtitle">Online 24/7</span>
+            </div>
+
+        </div>
+    )
 }
 
-export default Info
+export default Info;
