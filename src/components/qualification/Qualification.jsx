d3928cb2053f9e5456f2ff4e49d17b3f76bb95c4
Ullasachar16 <ullasachar16@gmail.com>
2023-06-24 13:11:57 +0530

    adding contents to qualification section

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import "./qualification.css";
 
 const Qualification = () => {
-  return (
-    <div>Qualification</div>
-  )
+    return (
+        <section className='qualification section'>
+            <h2 className='section_title'>Qualification</h2>
+            <span className="section_subtitle">My Personal Journey</span>
+
+            <div className="qualification_container container">
+                <div className="qualification_tabs">
+                    <div className="qualification_button qualification_active button--flex">
+                        <i className="uil uil-graduation-cap qualification_icon"></i>Education
+                    </div>
+
+                    <div className="qualification_button button--flex">
+                        <i className="uil uil-briefcase-alt qualification_icon"></i>Experience
+                    </div>
+                </div>
+
+                <div className="qualification_section">
+                    <div className="qualification_content">
+                        <div className="qualification_data">
+                            <h3 className="qualification_title"></h3>
+                            <span className="qualification_subtitle"></span>
+                            <div className="qualification_calendar">
+                                <i className="uil uil-calendar-alt"></i>2021 - Present
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Qualification;
