bba1df4794345986a0667741f97de72313fb344d
Ullas P <ullasachar16@gmail.com>
2023-06-28 12:10:24 +0530

    adding contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import "./contact.css";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <section className="contact section" id="contact">
+            <h2 className="section_title">Get in touch</h2>
+            <span className="section_subtitle">Contact Me</span>
+
+        </section>
+    )
 }
 
 export default Contact;
