272b5b0b82f7eabcae37c19e084b040b826dd492
Ullas P <ullasachar16@gmail.com>
2023-06-28 12:26:24 +0530

    adding contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,32 +13,32 @@ const Contact = () => {
 
                     <div className="contact_info">
                         <div className="contact_card">
-                            <i className="bx bx contact_card-icon"></i>
+                            <i className="bx bx-mail-send contact_card-icon"></i>
 
                             <h3 className="contact_card-title">Email</h3>
                             <span className="contact_card-data">user@gmail.com</span>
 
-                            <a href="" className="contact_button">Write me
+                            <a href="mailto:examplemail@gmail.com.com" className="contact_button">Write me
                                 <i className="bx bx-right-arrow-alt contact_button-icon"></i></a>
                         </div>
 
                         <div className="contact_card">
-                            <i className="bx bx contact_card-icon"></i>
+                            <i className="bx bxl-whatsapp contact_card-icon"></i>
 
                             <h3 className="contact_card-title">WhatsApp</h3>
                             <span className="contact_card-data">999-888-777</span>
 
-                            <a href="" className="contact_button">Write me
+                            <a href="https://api.whatsapp.com/send?phone=62214408789&text=Hello, more information!" className="contact_button">Write me
                                 <i className="bx bx-right-arrow-alt contact_button-icon"></i></a>
                         </div>
 
                         <div className="contact_card">
-                            <i className="bx bx contact_card-icon"></i>
+                            <i className="bx bxl-messenger contact_card-icon"></i>
 
                             <h3 className="contact_card-title">Messenger</h3>
                             <span className="contact_card-data">user.fb123</span>
 
-                            <a href="" className="contact_button">Write me
+                            <a href="https://m.me/crypticalcoder" className="contact_button">Write me
                                 <i className="bx bx-right-arrow-alt contact_button-icon"></i></a>
                         </div>
                     </div>
@@ -46,6 +46,10 @@ const Contact = () => {
 
                 <div className="contact_content">
                     <h3 className="contact_title">Write me your project</h3>
+
+                    <form className="contact_form">
+                        
+                    </form>
                 </div>
             </div>
         </section>
