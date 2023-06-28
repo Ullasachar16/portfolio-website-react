58f822edbfcc06c5030b2ce453aff89e89009a93
Ullas P <ullasachar16@gmail.com>
2023-06-28 11:08:45 +0530

    adding contents in testimonial section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import "./testimonials.css";
+import "./testimonial.css";
+import { Data } from './Data';
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className="testimonial container section">
+      <h2 className="section_title">My clients say</h2>
+      <span className="section_subtitle">Testimonial</span>
+
+      <div className="testimonial_container">
+        {Data.map(({id, image, title, description}) => {
+            return (
+              <div className="testimonial_card" key={id}>
+                <img src={image} alt="" className="testimonial_img" />
+                <h3 className="testimonial_name">{title}</h3>
+                <p className="testimonial_description">{description}</p>
+              </div>
+            )
+        })}
+      </div>
+    </section>
   )
 }
 
